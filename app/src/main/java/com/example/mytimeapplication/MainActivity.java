36baa02b8c98e774dc3160d6def009c5f8a4e5a9
Qiao Yu <691761071@qq.com>
2019-04-28 14:40:39 +0800

    first commit : frame of tabLayout and Toolbar

---
+++ app/src/main/java/com/example/mytimeapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.mytimeapplication;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import com.example.mytimeapplication.activity.HistoryFragment;
+import com.example.mytimeapplication.activity.NowFragment;
+import com.example.mytimeapplication.adapter.MyFragmentAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String []sTitle = new String[]{"HISTORY","NOW"};
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+    private void initView(){
+        mViewPager = findViewById(R.id.view_pager);
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        mTabLayout.addTab(mTabLayout.newTab().setText(sTitle[0]));
+        mTabLayout.addTab(mTabLayout.newTab().setText(sTitle[1]));
+
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(HistoryFragment.newInstance());
+        fragments.add(NowFragment.newInstance());
+
+        MyFragmentAdapter adapter = new MyFragmentAdapter(getSupportFragmentManager(),fragments, Arrays.asList(sTitle));
+        mViewPager.setAdapter(adapter);
+    }
+}
